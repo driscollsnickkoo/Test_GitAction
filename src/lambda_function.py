30faa79b680b54acd6fc0a26e4d06786a0a7b4ee
Nick Koo <nick.koo@driscolls.com>
2025-06-15 19:48:23 +0100

    with sns

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,9 +1,15 @@
+import boto3
 import json
 
 def lambda_handler(event, context):
+    sns = boto3.client('sns')
+    sns.publish(
+        TopicArn = 'arn:aws:sns:us-east-1:825765396866:QMPTableUpdateAlert',
+        Message = 'NK Test sns from Lambda Message',
+        Subject = 'NK TEst SNS from Lambda subject'
+    )
+
     
-
-
     return {
         'statusCode': 200,
         'body': json.dumps('new update on git zip')             
