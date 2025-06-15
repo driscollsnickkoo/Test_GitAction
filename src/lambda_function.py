abc503f0c75001a4c35381e5cf45f54ed313a4f3
Nick Koo <nick.koo@driscolls.com>
2025-06-15 20:48:26 +0100

    with sns and mysql test

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,17 +1,41 @@
 import boto3
+import os
 import json
+import pymysql
 
 def lambda_handler(event, context):
-    sns = boto3.client('sns')
-    sns.publish(
-        TopicArn = 'arn:aws:sns:us-east-1:825765396866:QMPTableUpdateAlert',
-        Message = 'NK Test sns from Lambda Message',
-        Subject = 'NK TEst SNS from Lambda subject'
-    )
+    try:
+        connection = pymysql.connect(
+            host=os.environ['DB_HOST'],
+            user=os.environ['DB_USER'],
+            password=os.environ['DB_PASS'],
+            db=os.environ['DB_NAME'],
+            port=int(os.environ.get('DB_PORT', 3306)),
+            connect_timeout=5
+        )
+        if connection:
+                sns = boto3.client('sns')
+                sns.publish(
+                    TopicArn = 'arn:aws:sns:us-east-1:825765396866:QMPTableUpdateAlert',
+                    Message = 'NK Test sns from Lambda Message',
+                    Subject = 'NK TEst SNS from Lambda subject'
+                )
+                connection.close()
+        return {
+            'statusCode': 200,
+            'body': json.dumps('✅ Connection to MySQL successful')
+        }
 
-    
-    return {
-        'statusCode': 200,
-        'body': json.dumps('new update on git zip')             
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f'❌ Connection failed: {str(e)}')
+        }
+
+
+
+    # return {
+    #     'statusCode': 200,
+    #     'body': json.dumps('new update on git zip')             
            
-    }
+    # }
