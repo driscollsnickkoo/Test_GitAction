a9ee889f987c74803ae8acab0fa302501a238325
Nick Koo <nick.koo@driscolls.com>
2025-06-20 14:54:28 +0100

    test sns

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,26 +1,41 @@
 import boto3
 import os
 import json
-import pymysql
+# import pymysql
 
-conn = None
+# conn = None
 
-def get_connection():
-    global conn
-    if conn is None or not conn.open:
-        conn = pymysql.connect(
-            host=os.environ['DB_HOST'],
-            user=os.environ['DB_USER'],
-            password=os.environ['DB_PASS'],
-            db=os.environ['DB_NAME'],
-            port=int(os.environ.get('DB_PORT', 3306)),
-            connect_timeout=5
-        )
-    return conn
+# def get_connection():
+#     global conn
+#     if conn is None or not conn.open:
+#         conn = pymysql.connect(
+#             host=os.environ['DB_HOST'],
+#             user=os.environ['DB_USER'],
+#             password=os.environ['DB_PASS'],
+#             db=os.environ['DB_NAME'],
+#             port=int(os.environ.get('DB_PORT', 3306)),
+#             connect_timeout=5
+#         )
+#     return conn
+
+sns_client = boto3.client('sns')
 
 def lambda_handler(event, context):
     try:
-        print("Trying to connect to:", os.environ['DB_HOST'])
+        topic_arn = os.environ['SNS_TOPIC_ARN']
+        message = {
+            "default": "Hello from Lambda via SNS!"
+            }
+        print(f'NK Testing sns arn{topic_arn} and message: {message}')
+
+        response = sns_client.publish(
+            TopicArn=topic_arn,
+            Message=json.dumps(message),
+            MessageStructure='json'
+        )
+
+        
+
         
         # connection = get_connection()
         # if connection:
@@ -33,7 +48,10 @@ def lambda_handler(event, context):
         #         connection.close()
         return {
             'statusCode': 200,
-            'body': json.dumps('✅ initial print helloworld')
+            'body': json.dumps({
+                'messageId': response['MessageId'],
+                'status': 'SNS notification sent'
+            })
         }
 
     except Exception as e:
