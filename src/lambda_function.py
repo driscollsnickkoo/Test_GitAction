d0cda990aae8ed0df61c7adcee7876e512ae9bb9
Nick Koo <nick.koo@driscolls.com>
2025-06-20 15:35:46 +0100

    update with new sns and lambda

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,57 +1,20 @@
 import boto3
-import os
 import json
-# import pymysql
-
-# conn = None
-
-# def get_connection():
-#     global conn
-#     if conn is None or not conn.open:
-#         conn = pymysql.connect(
-#             host=os.environ['DB_HOST'],
-#             user=os.environ['DB_USER'],
-#             password=os.environ['DB_PASS'],
-#             db=os.environ['DB_NAME'],
-#             port=int(os.environ.get('DB_PORT', 3306)),
-#             connect_timeout=5
-#         )
-#     return conn
 
 sns_client = boto3.client('sns')
 
 def lambda_handler(event, context):
     try:
-        topic_arn = os.environ['SNS_TOPIC_ARN']
-
-        sns_client.publish(
-                    TopicArn = 'arn:aws:sns:us-east-1:825765396866:QMPTableUpdateAlert',
-                    Message = 'NK Test sns from Lambda Message',
-                    Subject = 'NK TEst SNS from Lambda subject'
-                )
-
         response = sns_client.publish(
-            TopicArn=topic_arn,
-            Message=json.dumps('sns sent'),
-            MessageStructure='json'
+            TopicArn='arn:aws:sns:us-east-1:825765396866:QMPTableUpdateAlert',
+            Message='NK Test sns from Lambda Message',
+            Subject='NK Test SNS from Lambda subject'
         )
 
-        
-
-        
-        # connection = get_connection()
-        # if connection:
-        #         sns = boto3.client('sns')
-        #         sns.publish(
-        #             TopicArn = 'arn:aws:sns:us-east-1:825765396866:QMPTableUpdateAlert',
-        #             Message = 'NK Test sns from Lambda Message',
-        #             Subject = 'NK TEst SNS from Lambda subject'
-        #         )
-        #         connection.close()
         return {
             'statusCode': 200,
             'body': json.dumps({
-                'messageId': response['MessageId'],
+                'messageId': response.get('MessageId'),
                 'status': 'SNS notification sent'
             })
         }
@@ -61,11 +24,3 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps(f'❌ Connection failed: {str(e)}')
         }
-
-
-
-    # return {
-    #     'statusCode': 200,
-    #     'body': json.dumps('new update on git zip')             
-           
-    # }
